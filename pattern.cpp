8e700e147fb772e1726e3c4da695e9b72613f097
Anuj Singh Panwar <anujsinghpanwar07@gmail.com>
2026-02-08 00:56:32 +0530

    lecture 4

---
+++ pattern.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 4;
+    // for(int i = 1 ; i <=n; i++){//outer loop
+    //     for(int j = 1 ; j <= n ; j++){//inner loop
+    //         cout<<j <<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for (int i = 0; i < n; i++)//outer loop
+    // {
+    //     char ch = 'A';
+    //     for (int j = 0; j < n; j++)//inner loop
+    //     {
+    //         cout << ch<<" ";
+    //         ch = ch + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // int a = 1;
+    //  for(int i = 1 ; i <=n; i++){//outer loop
+    //     for(int j = 1 ; j <= n ; j++){//inner loop
+    //         cout<<a <<" ";
+    //         a = a+1;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int a = 0; // my code different from lecture
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j <= a; j++) // for(int j = 0; j < i+1 ; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     a = a + 1; // then there is no need for this
+    //     cout << endl;
+    // }
+
+    // int a=1;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j <i+1 ; j++)
+    //     {
+    //         cout<<a<<" ";//instead of a we can use i+1
+    //     }
+    //     a=a+1;
+    //     cout<<endl;
+    // }
+
+    // char ch='A';
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         for (int j = 0; j < i+1; j++)
+    //         {
+    //             cout<<ch;
+    //         }
+    //         ch=ch+1;
+    //         cout<<endl;
+    //     }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i+1; j++)
+    //     {
+    //         cout<<j+1;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i+1; j > 0; j--)
+    //     {
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int a=1;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i+1; j++)
+    //     {
+    //         cout<<a<<" ";
+    //         a++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // char ch ='A';
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i+1; j++)
+    //     {
+    //         cout<<ch<<" ";
+    //     }
+    //     ch++;
+    //     cout<<endl;
+    // }
+
+      for (int i = 0; i < n; i++) {
+        char ch = 'A' + i;   // starting character for the row
+
+        for (int j = i + 1; j > 0; j--) {
+            cout << ch;
+            ch--;
+        }
+        cout << endl;
+    }
+}
