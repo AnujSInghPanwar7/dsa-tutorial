904709665799016613c03c073237e604ebd22c1b
Anuj Singh Panwar <anujsinghpanwar07@gmail.com>
2026-07-09 23:43:00 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -4,7 +4,9 @@ int main()
 {
     int n = 4;
     int num = 0;
-char ch = 'A';
+    int numb = 4;
+
+    char ch = 'A';
     // Q) square patterns
     // number
 
@@ -12,10 +14,10 @@ char ch = 'A';
     {
         for (int j = 1; j <= n; j++)
         {
-            cout<<j<<" ";
+            cout << j << " ";
         }
 
-        cout<<endl;
+        cout << endl;
     }
 
     // Q) square patterns
@@ -26,9 +28,9 @@ char ch = 'A';
     {
         for (int j = 0; j < n; j++)
         {
-            cout<<"* ";
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
     // Q) square patterns
@@ -36,14 +38,14 @@ char ch = 'A';
 
     for (int i = 0; i < n; i++)
     {
-        char ch='A';
+        char ch = 'A';
         for (int j = 0; j < n; j++)
         {
 
-            cout<<ch<<" ";
-            ch=ch+1;
+            cout << ch << " ";
+            ch = ch + 1;
         }
-        cout<<endl;
+        cout << endl;
     }
 
     // Q) square pattern
@@ -54,9 +56,9 @@ char ch = 'A';
         for (int j = 0; j < n; j++)
         {
             num++;
-            cout<<num<<" ";
+            cout << num << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
     // Q) square pattern
@@ -67,10 +69,10 @@ char ch = 'A';
     {
         for (int j = 0; j < n; j++)
         {
-            cout<<ch<<" ";
+            cout << ch << " ";
             ch++;
         }
-        cout<<endl;
+        cout << endl;
     }
 
     // Q) triangle pattern
@@ -92,7 +94,7 @@ char ch = 'A';
     {
         for (int j = 0; j <= i; j++)
         {
-            cout << num + 1 << " ";//optimise krenge to num+1 ki jagah hum direct i+1 hi print kr sakte jise hume num ko fir baar baar ++ krne ki jarurat nahi hai
+            cout << num + 1 << " "; // optimise krenge to num+1 ki jagah hum direct i+1 hi print kr sakte jise hume num ko fir baar baar ++ krne ki jarurat nahi hai
         }
         num++;
         cout << endl;
@@ -103,38 +105,157 @@ char ch = 'A';
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i+1; j++)
+        for (int j = 0; j < i + 1; j++)
         {
-            cout<<ch<<" ";
+            cout << ch << " ";
         }
         ch++;
-        cout<<endl;
+        cout << endl;
     }
-    
+
     // Q)triangle pattern
     // same no se start hoga ek line mai but har line mai no increase hoga 📐
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i+1; j++)
+        for (int j = 0; j < i + 1; j++)
         {
-            cout<<j+1<<" ";
+            cout << j + 1 << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
-    //Q) reverse triangle pattern
-    //same no se end hoga jo ki decrease hoga start se ek line mai but har line mai no increase hoga 📐
+    // Q) reverse triangle pattern
+    // same no se end hoga jo ki decrease hoga start se ek line mai but har line mai no increase hoga 📐
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = i+1; j > 0; j--)
+        for (int j = i + 1; j > 0; j--)
         {
-            cout<<j<<" ";
+            cout << j << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-    
-    
+    // Q) floyd's triangle pattern
+    // number
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            num++;
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
+    // Q) floyd's triangle pattern
+    // character
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+
+    // Q) inverted triangle pattern
+    // number
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "  ";
+        }
+        for (int k = n; k > i; k--)
+        {
+            cout << i + 1 << " ";
+        }
+
+        cout << endl;
+    }
+
+    // Q) inverted triangle pattern
+    // character
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "  ";
+        }
+        for (int k = n; k > i; k--)
+        {
+            cout << ch << " ";
+        }
+        ch++;
+
+        cout << endl;
+    }
+
+    // Q) pyramid pattern
+    // number
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n; j > i + 1; j--)
+        {
+            cout << "  ";
+        }
+        for (int k = 0; k < i + 1; k++)
+        {
+            cout << k + 1 << " ";
+        }
+        for (int l = i + 1; l > 1; l--)
+        {
+            cout << l - 1 << " ";
+        }
+
+        cout << endl;
+    }
+
+    // Q) hollow diamond pattern
+    // diamond on ends
+
+    for (int i = 0; i < n; i++) // outer loop
+    {
+        for (int j = n - i - 1; j > 0; j--) // inner loop for 1st space
+        {
+            cout << " ";
+        }
+        cout << "*";
+        for (int k = 0; k < 2 * i - 1; k++) // inner loop for between space
+        {
+            cout << " ";
+            if (k == 2 * i - 2) // taki sirf end mai aaye
+            {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        for (int k = n - 2 * i - 1; k > 0; k--)
+        {
+            cout << " ";
+            if (k == 1)
+            {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+
     return 0;
 }
